034e260fa264c62c3359deabbe4d29d48ddb4cb4
Juan Hurtado <juanhurtado4@outlook.com>
2018-04-11 07:23:41 -0700

    Docs: Add documentation

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+/*
+ * Title: Copy UI Feature with Animation
+ * Description: Example of a useful feature to provide the user to copy from a click of a button, rather than to highlight, then right click, then copy. Ghosting animation is used to provide feedback that item was copied.
+ * Author: Juan Hurtado
+ * Date: 04/11/2018
+ *
+ */
+
 const $input = $("input");
 
 $("button").click(() => {
   let $copyTxt = $("#textToCopy");
+
+  // Grab the cordinates of the text to be copy
+  // Will be used to place a duplicate text over this one for animation
   let copyTxtCor = {
     height: $copyTxt.innerHeight(),
     width: $copyTxt.innerWidth(),
     top: $copyTxt.position().top,
     left: $copyTxt.position().left
   }
+  // Create a duplicate element and give cordinates directly over the original #textToCopy
   let $copyTxtFade = $("<p></p>").text($copyTxt.text()).css({
     "height": copyTxtCor.height,
     "width": copyTxtCor.width,
@@ -15,18 +27,18 @@ $("button").click(() => {
     "left": copyTxtCor.left,
     "position": "absolute"
   })
-  console.log($copyTxtFade.width());
-  console.log(copyTxtCor.width);
 
+  // Insert duplicate text in the dom
   $copyTxt.after($copyTxtFade);
 
-  // $copyTxt.addClass("juanAni");
   $copyTxtFade.addClass("juanAni");
 
-  // $input.val($copyTxt.text()).select();
+  // Grab the text and insert it into a non visible input field
+  // To be able to copy to the user's clipboard
   $input.val($copyTxtFade.text()).select();
   document.execCommand("Copy");
 
+  // Wait one second for animation to complete before removing class
   setTimeout(() => {
   $copyTxtFade.remove();
   }, 1000);
