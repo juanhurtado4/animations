e7368a8ab1d09e8c4bf02b53125e169110af6d52
Juan Hurtado <juanhurtado4@outlook.com>
2018-04-22 18:44:40 -0700

    Feat: Add JS for DOM manipulation

---
+++ shape_shifter/index.js
@@ -0,0 +1,20 @@
+/*
+ * Shape shifting animations
+ * Author: Juan Hurtado
+ */
+
+let shape = document.querySelector(".square");
+const shapes = ['square', 'rectangle', 'circle'];
+
+let nextIndex = 0;
+shape.addEventListener('click', () => {
+  // Remove styling for current shape
+  shape.classList.remove(shapes[nextIndex]);
+
+  // Pick next shape without going out of bounds in the arr
+  nextIndex = nextIndex === shapes.length - 1 ? 0 : nextIndex + 1
+
+  // Create next shape
+  shape.classList.add(shapes[nextIndex]);
+
+});
